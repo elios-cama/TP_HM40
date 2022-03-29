93057acbe770bfa64c423949f3295f80790e4365
ydufal <yann.dufale@utbm.fr>
2022-03-29 17:14:45 +0200

    new version

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -4,7 +4,12 @@
 
 int main(int argc, char *argv[])
 {
+
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    // Enables high-DPI scaling in Qt on supported platforms
+
     QApplication a(argc, argv);
+    a.setApplicationName("FittsInterface");
 
     FittsView *Mainwindow = new FittsView ;
     Mainwindow->show() ;
